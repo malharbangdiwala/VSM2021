7001969dcd5c094dd7b09a43ba59402489cebec5
malharbangdiwala <malharbangdiwala03@gmail.com>
2020-12-28 16:15:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ConnectionHelper.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+
+
+    @SuppressLint("NewApi")
+    public static Connection CONN() {
+
+        String _user = "malhar";
+        String _pass = "Divya@1234";
+        String _DB = "VSM21";
+        String _server = "vsmocculus.database.windows.net";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnURL = "jdbc:jtds:sqlserver://" + _server + ";"
+                    + "databaseName=" + _DB + ";user=" + _user + ";password="
+                    + _pass + ";";
+            conn = DriverManager.getConnection(ConnURL);
+            Log.i("Info","success");
+        } catch (SQLException se) {
+            Log.e("ERROR", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERROR", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage());
+        }
+        return conn;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.media.tv.TvContract;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            ConnectionHelper con = new ConnectionHelper();
+            Connection connect = ConnectionHelper.CONN();
+            Log.i("In", "successss");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
