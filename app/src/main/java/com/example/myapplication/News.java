3778e3358c1e6d5aa8cabc7f5a2b45e918fa6464
malharbangdiwala <malharbangdiwala03@gmail.com>
2021-02-25 21:36:47 +0530

    Changed news

--- app/src/main/java/com/example/myapplication/News.java
+++ app/src/main/java/com/example/myapplication/News.java
@@ -43,10 +43,10 @@ public class News {
         if(day==1) {
             switch (HomeFragment.roundNo) {
                 case 1:
-                    setNews.add("Scientist discover 119th element <name> and that has super strength and is light weighted and as malleable as steel and is environment friendly. To get this functional scientist need heavy use of Chemical refineries.");
-                    setNews.add("Government has decided to privatise the Airline Sector, which had bidders from various major leagues of Utopia.");
-                    setNews.add("Pharma Giant Dr. Utopia receives overseas funding and looks forward to expand their business to different domains.");
-                    setNews.add("World’s largest economy has an election coming up and there is a massive support for Xi Zie  from conservative party.");
+                    setNews.add("The 127th element of the periodic table is equally strong and malleable but requires high end chemicals refineries.");
+                    setNews.add("Airline Sector may soon be privatized.");
+                    setNews.add("Dr.Pharma is looking for foreign investors.");
+                    setNews.add("Chances for the conservative party to win the elections seems pretty high.");
                     if (PowerCardFragment.pc2flag == 1) {
                         setNews.add(0, "Union of IT workers plans to declare strike due to long working hours and increasing deadline pressure.");
                         PowerCardFragment.pc2flag = 0;
@@ -54,41 +54,50 @@ public class News {
                     break;
                 case 2:
                     setNews.clear();
-                    setNews.add("Government awards tendor to Dr. Utopia for privatisation of Air Utopia.");
-                    setNews.add("Election Results are OUT , Xi Zie wins the throne with an overwhelming majority. His first action plan is to revise the policy on Immigration of Utopia Workers and reduce investment into Utopian MSE.");
-                    setNews.add("Students from SPIT are developing a game changing AI to smoothen the cryptocurrency transactions.");
+                    setNews.add("Dr.Pharma owns a majority stake in Air Utopia after they win the tender.");
+                    setNews.add("Xi Zie wins the elections with an overwhelming majority.");
+                    setNews.add("Xi Zie plans is to revise the policy on Immigration of Utopia Workers and reduce investment into Utopian MSE.");
+                    setNews.add("AI to smoothen the cryptocurrency transactions is being developed.");
                     if (PowerCardFragment.pc2flag == 1) {
-                        setNews.add(0, "Friction between board member of Utopia Bank regarding their upcoming policy <name> to reduce the repo rate by 2%.");
+                        setNews.add(0, "Friction between board members of Utopia Bank regarding their upcoming policy “Ease of Loan” to reduce the repo rate by 2%.");
                         PowerCardFragment.pc2flag = 0;
                         newscolorflag = 1;
                     }
                     break;
                 case 3:
                     setNews.clear();
-                    setNews.add("Researchers found that name is hazardous to human life in the longer run and chemical research is required to control the radiation caused by existing element.");
-                    setNews.add("Dr. Utopia conducts a survey and devices a plan to neutralize humans of the radiations and gets huge sums of money for the research.");
-                    setNews.add("Utopia Banks policy name to reduce the repo rate gets consensus.");
-                    setNews.add("UTOPIA backs out of Paris Agreement due to Xi Zie’s conservative approach.");
+                    setNews.add("Ununbaktoniom is highly radioactive and causes damage to human life.");
+                    setNews.add("Dr. Pharma devises a plan to neutralize humans of these radiations.");
+                    setNews.add("Dr. Pharma gets a huge grant for research regarding neutralizing these radiations.");
+                    setNews.add("Bank’s policy “Ease of Loan” to reduce the repo rate gets consensus.");
+                    setNews.add("Utopia backs out of the Paris Agreement due to Xi Zie’s conservative approach.");
                     if (PowerCardFragment.pc2flag == 1) {
-                        setNews.add(0, "UTOPIA Investments planning to increase their mutual funds share in Air Utopia to 29.33% from 21%.");
+                        setNews.add(0, "Utopia Investmentor Pvt Limited planning to increase their mutual funds share in Air Utopia to 29.33% from 21%.");
                         PowerCardFragment.pc2flag = 0;
                         newscolorflag = 1;
                     }
                     break;
                 case 4:
                     setNews.clear();
-                    setNews.add("SPITian’s make UTOPIA proud after successfully creating the greatest ever AI technology single handedly.");
-                    setNews.add("In the latest audit, Dr. Utopia gets accused of allegedly involved in diverting funds received for research in subsidiary companies.");
+                    setNews.add("AI to smooth cryptocurrency is ready!!");
+                    setNews.add("Dr. Pharma accused of being involved in diverting funds in subsidiary companies.");
                     setNews.add("Government incentivises the use of Solar Panels.");
-                    setNews.add("Banks witness in significant increase in number of NPA(non-performing assets).");
+                    setNews.add("Increase in the number of NPA(non-performing assets) for Banks.");
                     if (PowerCardFragment.pc2flag == 1) {
-                        setNews.add(0, "UTOPIA plans to sign Paris Agreement again due to backlash by other governments.");
+                        setNews.add(0, "The Fintech industry is about to boom.");
                         PowerCardFragment.pc2flag = 0;
                         newscolorflag = 1;
                     }
                     break;
                 case 5:
+                    setNews.clear();
+                    setNews.add("Tech Giant opens a fintech subsidiary Finology.");
+                    setNews.add("Finology has become the most popular brand to learn and invest in the share market.");
+                    setNews.add("Utopians are free from all radiations caused by Ununbaktoniom.");
+                    setNews.add("Xi Zie signs a peace treaty with Utopia.");
+                    setNews.add("Another steel mine discovered in Utopia.");
                     if (PowerCardFragment.pc2flag == 1) {
+                        setNews.add(0, "Utopia plans to sign Paris Agreement again due to backlash by other governments.\n");
                         PowerCardFragment.pc2flag = 0;
                     }
                     break;
